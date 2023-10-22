296aff56fc5390ffea1add9cbeba15bda173d364
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-10-22 12:46:44 +0000

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -3,53 +3,38 @@ import "bootstrap";
 import "./style.css";
 
 //write your code here
-
-var who = ["The dog", "My grandma", "His turtle", "My bird"];
-var action = ["ate", "peed", "crushed", "broke"];
-var what = ["my homework", "the keys", "the car"];
-var when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
-
-function generateElementPosition(array) {
-  console.log("get element");
-  console.log("select a random element from any array");
-
-  return Math.floor(Math.random() * array.length);
-}
-
-window.onload = function generateExcuse() {
-  who[generateElementPosition(who)] +
-    action[generateElementPosition(action)] +
-    what[generateElementPosition(what)] +
-    when[generateElementPosition(when)];
-
-  let excuse = getElementbyId("excuse");
-  excuse.innerHTML = generateExcuse;
+window.onload = function() {
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
 
-/*
-function generateExcuse() {
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-    const randomWho = who[Math.floor(Math.random() * who.length)];
-    const randomAction = action[Math.floor(Math.random() * action.length)];
-    const randomWhat = what[Math.floor(Math.random() * what.length)];
-    const randomWhen = when[Math.floor(Math.random() * when.length)];
-  
-   return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+  let randomWho = getRandomElement(who);
+  let randomAction = getRandomElement(action);
+  let randomWhat = getRandomElement(what);
+  let randomWhen = getRandomElement(when);
+
+  return (
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen]
+  );
 }
 
-// Genera la excusa y actualiza el contenido del elemento
-const randomExcuse = generateExcuse();
-excuseElement.textContent = randomExcuse;
-
-/*
-// Función para generar el random de cada array
-function random (){
-return aleatorio.random();
+function getRandomElement(ArrayCalcular) {
+  return Math.floor(Math.random() * ArrayCalcular.length);
 }
-*/
